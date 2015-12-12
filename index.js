a050c1c249c56bd882ee8a047aab77f4d87ab7a4
Neal Shyam <neal@audioshocker.com>
2015-12-11 22:56:59 -0500

    whatever.

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 /*
 
-USE THIS SCRIPT TO GENERATE YOUR OWN CRYPTOPOSTAL PAGE.
+Use this script to create your own cryptopostal page. Edit the variables below, run `node cryptopostal.js`, and copy all the files (except this one) over to your webhost.
 
-BTW, DO NOT DISTRIBUTE THIS FILE, COMMIT YOUR ADDRESS TO GIT, OR SHOW PEOPLE YOUR PASSPHRASE ALONG WITH ENCRYPTED DATA. OTHERWISE THIS IS ALL TOTALLY POINTLESS!
+BTW, DO NOT DISTRIBUTE THIS FILE, COMMIT YOUR UNENCRYPTED ADDRESS TO GIT, OR SHOW PEOPLE YOUR PASSPHRASE -- OTHERWISE THIS IS ALL TOTALLY POINTLESS!
+
+@nealrs 2015
 
 */
 
-//////
+var name = "Doofus"; // person name
+var avatar = "https://media.giphy.com/media/RFdqSN1xVYJkA/giphy.gif"; // path to headshot (local or remote)
+var q1 = "Sting's crush:"; // first question (should be a name)
+var q2 = "Jenny's number:"; // second question (should be a numeric string (no spaces/non numeric characters))
+var pass = "roxanne8675309"; // your passphrase (e.g. combo of your name + sanitized phone #)
+var d1 = "sup3rs3kr1t"; // test phrase
+var d2 = "1060 W Addison St.<br>Chicago, IL 60613"; // street address -- leave the <br> in there.
+var d3 = "https://www.google.com/maps/place/Wrigley+Field/@41.9484384,-87.6553327,15z/data=!4m2!3m1!1s0x0:0x1cea3ee176ddd646?sa=X&ved=0ahUKEwjBoIy_tNXJAhXFpB4KHY0YCcYQ_BIIhQEwCw"; //map link
+
+
+/*
+
+DON'T EDIT ANYTHING BELOW
+
+*/
+
 var sjcl = require("sjcl");
 var m = require ("mustache");
 var fs = require('fs');
-//////
-
-var name = "Devpost"; // person/org name
-var avatar = "img/devpost.png"; // path to headshot
-var pass = "jessie5558675309"; // your passphrase (e.g. combo of your name + sanitized phone #)
-var testphrase = "f00bar"; // test phrase
-var d2 = "433 West 14th Street, Suite 3F<br>New York, NY 10014"; // street address -- leave the <br> in there.
-var d3 = "https://www.google.com/maps/place/433+W+14th+St+%233f,+New+York,+NY+10014/data=!4m2!3m1!1s0x89c259c09cab1123:0xa6beacfb13dbfcb1?sa=X&ved=0ahUKEwif3amt8dLJAhUHKx4KHU4HCuMQ8gEIGzAA"; //map link
 
 // encrypted data
 e1 = sjcl.encrypt(pass, d1);
 e2 = sjcl.encrypt(pass, d2);
 e3 = sjcl.encrypt(pass, d3);
 
-// write index.html
+// render template
 fs.readFile('template.html', function (err, data) {
   if (err) throw err;
   var html = m.render(data.toString(), {
-    name = name,
-    avatar = avatar,
-    pass = pass,
-    testphrase = testphrase,
-    e1 = e1,
-    e2 = e2,
-    e3 = e3
+    name : name,
+    avatar : avatar,
+    q1 : q1,
+    q2 : q2,
+    pass : pass,
+    testphrase : d1,
+    e1 : e1,
+    e2 : e2,
+    e3 : e3
   });
 
+  // save to disk (index.html)
   fs.writeFile("index.html", html, function(err) {
     if(err) {
       return console.log(err);
@@ -44,16 +56,3 @@ fs.readFile('template.html', function (err, data) {
     console.log("cryptopostal created");
   });
 });
-
-
-/*
-// now you can set e1, e2, e3 in index.html
-console.log("set e1 in your script to: " + e1+'\n');
-console.log("set e2 in your script to: " + e2+'\n');
-console.log("set e3 in your script to: " + e3+'\n');
-
-// and the proof is in the pudding!
-console.log("and here's your test phrase, decrypted: " + sjcl.decrypt(pass, e1)+'\n');
-console.log("and here's your address, decrypted: " + sjcl.decrypt(pass, e2)+'\n');
-console.log("and here's your map link, decrypted: " + sjcl.decrypt(pass, e3)+'\n');
-*/
